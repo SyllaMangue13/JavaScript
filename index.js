733b13c80285bc2a55e66289dc2d6bd8f3ad4f46
SyllaMangue13 <manguemasysylla13@gmail.com>
2022-07-27 12:17:03 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+"use strict";
+
+// let hasLi = false;
+// const pastest = true;
+
+// if(pastest) haLi = true ;
+// if(hasLi) console.log("i can drive");
+
+// hello();
+
+//  const sylla = function(myName) {
+//      return myName+2;
+// }
+
+// alert(sylla(2));
+
+// function hello(){
+//     alert("hello sylla");
+// }
+
+//  const sylla = new Array(11,20);
+
+// Add elements
+//  sylla.push("sylla");
+//  sylla.unshift("Mangue");
+//  sylla.pop();
+//  console.log(sylla);
+// sylla.unshift("liste11","liste1", "liste2", 3);
+// console.log(sylla);
+// console.log(sylla.indexOf('liste1'));
+
+// const sylla = {
+//    age: 21,
+//    name: "Sylla",
+//    secondName: "Mangue",
+//    sport:"foot"
+// }
+// console.log(sylla);
+let info = prompt("please enter your firdt name here");
+console.log(info);
+
+const x = 23;
